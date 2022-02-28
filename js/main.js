6d919ff91d24602474c3d79784eb1c16fc672848
marcinde1998 <marcinde1998@gmail.com>
2022-02-28 20:27:25 +0100

    jeden

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let liczba = 12;
+var kominikat = ('Podana liczba nie jest większa od 10');
+
+if (liczba > 10){
+    console.log('Podana liczba jest większa od 10');
+} else {
+    console.log(kominikat);
+}
